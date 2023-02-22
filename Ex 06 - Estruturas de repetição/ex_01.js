a8788c2e7ec8c31fc54d0dc3822fa78ab870f35e
Itiel Dario <itiel.dario@gmail.com>
2023-02-22 19:02:19 -0300

    Criação da pasta estruturas de repetição

---
+++ Ex 06 - Estruturas de repetição/ex_01.js
@@ -0,0 +1,33 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+// WHILE ---------------------------------------------------------------------------------
+/*var c = 0
+while(c < 6){
+    c++
+    console.log(`Tudo bem?`)
+}
+console.log(``)
+
+
+// DO / WHILE ------------------------------------------------------------------------------
+var c = 0
+do{
+    c++
+    console.log(`Tudo bem?`)
+}while(c < 6)
+console.log(``)
+*/
+
+// FOR ------------------------------------------------------------------------------------
+console.log('Vai começar ...')
+for(var c = 0; c < 6; c++){
+    console.log(`Tudo bem?`)
+}
+console.log('FIM!')
