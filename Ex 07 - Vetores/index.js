3e5dca6040d64bca68028c9c519d0fdee731236e
Itiel Dario <itiel.dario@gmail.com>
2023-02-22 19:02:42 -0300

    Criação da pasta vetores

---
+++ Ex 07 - Vetores/index.js
@@ -0,0 +1,28 @@
+let num = [5, 8, 2, 9, 3]
+num[5] = 7
+num.push(1)  // ACRESCENTA UM INDICE NO FINAL COM O VALOR ENTRE ()
+num.sort()  // ORDENA O VETOR DO MENOR PARA O MAIOR ELEMENTO
+console.log(`O vetor é: ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é: ${num[0]}`)
+console.log(``)
+
+let valores = [32, 47, 8, 12, 29]
+console.log(valores)
+for(let pos = 0; pos < valores.length; pos++){
+    console.log(`A posição [${pos}] tem o valor: ${valores[pos]}`)
+}
+console.log(``)
+
+for(let pos in valores){  // PARA CADA POSIÇÃO (for(let pos) DENTRO (in) DE 'valores' -> SÓ FUNCIONA PARA VETORES
+    console.log(`A posição [${pos}] tem o valor: ${valores[pos]}`)
+}
+console.log(``)
+
+let pos = valores.indexOf(13) //RETORNA O INDICE DE ACORDO COM O VALOR ENTRE ()
+if(pos == -1){
+    console.log(`O valor nao foi encontrado`)  //RETORNA -1 POIS NAO ENCONTROU O VALOR ENTRE ()
+}
+else{
+    console.log(`Indice onde está o valor 12: [${valores.indexOf(12)}]`)  // NESTE CASO RETORNARA O INDICE 3
+}
