5ca36de7291125b0118abe0e8f9bc56be9577c8d
Itiel Dario <itiel.dario@gmail.com>
2023-02-27 22:22:05 -0300

    Update ex_01.js

--- Exercicios/Ex 06 - Estruturas de repetição/ex_01.js
+++ Exercicios/Ex 06 - Estruturas de repetição/ex_01.js
@@ -7,6 +7,7 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 */
 
+
 // WHILE ---------------------------------------------------------------------------------
 /*var c = 0
 while(c < 6){
@@ -25,9 +26,29 @@ do{
 console.log(``)
 */
 
+
 // FOR ------------------------------------------------------------------------------------
 console.log('Vai começar ...')
-for(var c = 0; c < 6; c++){
+for(var c = 0; c < 3; c++){
     console.log(`Tudo bem?`)
 }
-console.log('FIM!')
+console.log('')
+
+
+// FOR IN ---------------------------------------------------------------------------------
+// RETORNA A POSIÇÃO
+let num = [10, 20, 30, 40, 50]
+
+for(n in num){  // A VARIÁVEL 'n' RECEBE A POSIÇÃO DE CADA ELEMENTO DE 'num'
+    console.log(num[n])
+}
+console.log('')
+
+
+// FOR OF ---------------------------------------------------------------------------------
+// RETORNA O VALOR
+let numero = [10, 20, 30, 40, 50]
+
+for(n of num){  // A VARIÁVEL 'n' RECEBE O VALOR DE CADA ELEMENTO DE 'num'
+    console.log(n)
+}
