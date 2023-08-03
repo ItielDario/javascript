7317e738fad1752d5160122d0aa057d9ad79279d
Itiel Dario <itiel.dario@gmail.com>
2023-08-03 18:03:46 -0300

    Criando o mini-projeto 01

---
+++ Exercicios/Ex 14 - POO 2.0/Mini projeto 01/modificador.js
@@ -0,0 +1,54 @@
+// ESTE ARQUIVO PODE SER UTILIZADO PARA ORDENAR E CAPITALIZAR QUALQUER VETOR DE STRINGS. 
+// COLOCAMOS EM UM MÓDULO PARA QUE QUANDO PRECISARMOS É SO IMPORTAR E TEREMOS ACESSO AS FUNÇÕES.
+
+function funcCapitalizarNovo(colecao, atributo){
+    if(typeof colecao[0] == 'object'){  // MANIPULA VETORES COM OBJETOS
+        var resultado = colecao.map(function(obj) {
+            let letraInicial = obj[atributo].charAt(0).toUpperCase(); 
+            let restoTexto = obj[atributo].slice(1);   
+            obj[atributo] = letraInicial+ restoTexto;
+
+            return obj;
+        });
+
+        return resultado;
+    }
+    else{ // MANIPULA VETORES COM STRINGS
+        
+        var resultado = colecao.map(function(palavra){
+            let letraInicial = palavra.charAt(0); 
+            let letraAlta = letraInicial.toUpperCase();  
+            
+            let restoTexto = palavra.slice(1); 
+            let palavraFormada = letraAlta + restoTexto;  
+
+            return palavraFormada;
+        });
+
+        return resultado;
+
+        console.log(resultado);
+    }
+};
+
+function funcOrdenar(vetor, atributo){
+    return vetor.sort(function(a, b){ 
+        return a[atributo].localeCompare(b[atributo]); 
+    });    
+}
+
+function funcCaixaAlta(vetor){
+    var modificado = [];
+
+    for(let i = 0; i < vetor.length; i++){
+        modificado[i] = vetor[i].toUpperCase();
+    };
+
+    return modificado;
+}
+
+export default {  // OBJETO LITERAL
+    capitalizarNovo:  funcCapitalizarNovo,  // NOME: FUNÇÃO
+    ordenar: funcOrdenar, // NOME: FUNÇÃO
+    caixaAlta: funcCaixaAlta
+}
