141d627dd6eb6c6411537805efbf6c5c100a2333
Itiel Dario <itiel.dario@gmail.com>
2023-08-07 18:40:29 -0300

    Criando o pop-up

--- Desafios/Desafio - Pizzaria da Nonna/js/script.js
+++ Desafios/Desafio - Pizzaria da Nonna/js/script.js
@@ -1,14 +1,13 @@
 import pizzaJson from './pizzas.js';
 
 const containerPizza = document.querySelector('#group-pizzas');
-console.log(containerPizza)
 
 for (const pizza of pizzaJson) {
     let boxPizza = document.createElement('div');
     boxPizza.classList = 'box-pizza'
     boxPizza.innerHTML = `
         <img src="${pizza.img}" class="img-pizza" id=${pizza.id}>
-        <div class="btn-adicionar" id=${pizza.id}>+</div>
+        <div class="btn-adicionar" src="${pizza.img}" id=${pizza.id}>+</div>
         <p class="preco">R$ ${pizza.price},00</p>
         <h3 class="nome">${pizza.name}</h3>
         <p class="descricao">${pizza.description}</p>
@@ -16,7 +15,6 @@ for (const pizza of pizzaJson) {
 
     containerPizza.appendChild(boxPizza);
 }
-
 
 let pizzaImg = document.getElementsByTagName("img");
 let pizzaBtnAdicionar = document.getElementsByClassName("btn-adicionar");
@@ -29,3 +27,24 @@ for(let i = 0; i < pizzaImg.length; i++){
     elBtn.addEventListener('click', exibirModal);
 }
 
+function exibirModal(event){
+    let elementId = event.target.id;
+    let ObjElement = pizzaJson[elementId - 1]
+    document.querySelector('main').style.opacity = 0.3;
+    let popUp = document.createElement('aside');
+
+    popUp.classList = 'popUp';
+    popUp.innerHTML = `
+        <div class="content-pop">
+            <div class="img-pop">
+                <img src="${ObjElement.img}">
+            </div>
+
+            <div class="info-pop">
+                <h3>aaaaaaaaaaaa<h3>
+            </div>
+        </div>
+    `;
+    document.body.appendChild(popUp);   
+
+}
